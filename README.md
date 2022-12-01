a69ea337ac2a0cc86fa0d0f0adc89901c4b41da2
abrasumente999 <katieoliviaholliday@gmail.com>
2022-12-01 23:43:32 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Advent of Code 2022
+
+This repo contains my Javascript solutions to the [Advent of Code](https://adventofcode.com/) puzzles. 🎄
+
+Each folder will contain an index file containing two solutions, one for each task.
+
+# How to run
+
+Navigate to the day folder
+
+```
+npm install
+```
+
+Then
+
+```
+node index.js
+```
---
+++ day 01/index.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+const addCalories = () => {
+  const data = fs.readFileSync("input.txt", "utf8").split(/\n\s*\n/);
+  const newArr = [];
+
+  data.forEach((element) => {
+    newArr.push(element.split(/\n/));
+  });
+
+  return newArr.map((nested) => {
+    return nested.reduce((acc, curr) => +acc + +curr, 0);
+  });
+};
+
+console.log("Total calories carried by each elf:", addCalories());
+
+//
+//Task 1 - find the highest total number of calories carried by one elf
+const mostCalories = () => {
+  const totalCals = addCalories();
+  return Math.max(...totalCals);
+};
+
+console.log("Highest number of calories carried by one elf:", mostCalories());
+
+//
+//Task 2 - find the total number of calories carried by the top 3 elves
+const topThreeMostCalories = () => {
+  const totalCals = addCalories().sort((a, b) => b - a);
+  return totalCals.slice(0, 3).reduce((acc, curr) => acc + curr, 0);
+};
+
+console.log(
+  "Total number of calories carried by the top 3 elves:",
+  topThreeMostCalories()
+);
